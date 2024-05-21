afc26c2cc9502b5bef8817b3b6466430fbe76a05
Azhar <azharshaikh33@gmail.com>
2024-05-21 17:52:28 +0400

    Auto Push

--- route-table.tf
+++ route-table.tf
@@ -49,3 +49,10 @@ resource "aws_route_table_association" "private-rt-association" {
   subnet_id      = element(aws_subnet.PRIVATE_SUBNET.*.id, count.index)
   route_table_id = aws_route_table.private_rt.id
 }
+
+resource "aws_route" "default-vpc-route-table-robot" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = [aws_vpc_peering_connection.peer.id]
+  depends_on = [aws_vpc_peering_connection.peer]
+  }
