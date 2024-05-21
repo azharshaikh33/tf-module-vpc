611ad4a77658f05c2511ca8e01c015159e9421f3
Azhar <azharshaikh33@gmail.com>
2024-05-21 09:23:24 +0400

    Auto Push

---
+++ route-table.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    gateway_id = aws_vpc_peering_connection.peer.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
