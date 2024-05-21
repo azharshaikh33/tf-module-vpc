d8538b6b6cdb005e35f3057af77aa461a07af13f
Azhar <azharshaikh33@gmail.com>
2024-05-21 07:26:18 +0400

    Auto Push

--- subnets.tf
+++ subnets.tf
@@ -8,3 +8,14 @@ resource "aws_subnet" "PUBLIC_SUBNET" {
     Name = "ROBOT_PUBLIC_SUBNET-${element(var.AZ, count.index)}"
   }
 }
+
+resource "aws_subnet" "PRIVATE_SUBNET" {
+  count = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  availability_zone = element(var.AZ, count.index)
+
+  tags = {
+    Name = "ROBOT_PRIVATE_SUBNET-${element(var.AZ, count.index)}"
+  }
+}
