704b43100d95530b238f69d3e9d290ffe33835c6
Azhar <azharshaikh33@gmail.com>
2024-05-21 09:44:54 +0400

    Auto Push

--- route-table.tf
+++ route-table.tf
@@ -12,6 +12,11 @@ resource "aws_route_table" "public-rt" {
   }
 
   tags = {
-    Name = "public-rt"
+    Name = "roboshop-${var.ENV}public-rt"
   }
 }
+
+# resource "aws_route_table_association" "a" {
+#   subnet_id      = aws_subnet.foo.id
+#   route_table_id = aws_route_table.bar.id
+# }
