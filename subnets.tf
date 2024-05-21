de196d23a5af5164edab8b108cda0b7ba5ba5aa5
Azhar <azharshaikh33@gmail.com>
2024-05-21 07:21:26 +0400

    Auto Push

--- subnets.tf
+++ subnets.tf
@@ -2,8 +2,9 @@ resource "aws_subnet" "PUBLIC_SUBNET" {
   count = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone = element(var.AZ, count.index)
 
   tags = {
-    Name = "ROBOT_PUBLIC_SUBNET"
+    Name = "ROBOT_PUBLIC_SUBNET-${element(var.AZ, count.index)}"
   }
 }
