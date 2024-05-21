ec98ba9d59c25ebbe896ac06bdff615d0fefd047
Azhar <azharshaikh33@gmail.com>
2024-05-21 17:13:56 +0400

    Auto Push

--- igw.tf
+++ igw.tf
@@ -5,3 +5,24 @@ resource "aws_internet_gateway" "igw" {
     Name = "roboshop-${var.ENV}-igw"
   }
 }
+
+resource "aws_eip" "ngw_ip" {
+    vpc   = true
+
+    tags = {
+        Name = "roboshop-${var.ENV}-ngw_eip"
+    }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw_ip.id
+  subnet_id     = aws_subnet.PUBLIC_SUBNET.*.id[0]
+
+  tags = {
+    Name = "roboshop-${var.ENV}-ngw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.ngw_ip]
+}
--- route-table.tf
+++ route-table.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table" "public-rt" {
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -16,7 +16,36 @@ resource "aws_route_table" "public-rt" {
   }
 }
 
-# resource "aws_route_table_association" "a" {
-#   subnet_id      = aws_subnet.foo.id
-#   route_table_id = aws_route_table.bar.id
-# }
+resource "aws_route_table_association" "public-rt-association" {
+  count          = length(aws_subnet.PUBLIC_SUBNET.*.id)
+
+  subnet_id      = element(aws_subnet.PUBLIC_SUBNET.*.id, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# private route table
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_internet_gateway.ngw.id
+  }
+
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}private-rt"
+  }
+}
+
+resource "aws_route_table_association" "public-rt-association" {
+  count          = length(aws_subnet.PRIVATE_SUBNET.*.id)
+
+  subnet_id      = element(aws_subnet.PRIVATE_SUBNET.*.id, count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
